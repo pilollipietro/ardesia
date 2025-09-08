pilollipietro/ardesia

commit e011aaba6291c704820b19f3062a573a0a70adea

Bulk changes by Tom McCallum

src/annotation_window.h
@@ -1,4 +1,4 @@
-/* 
+/*
  * Ardesia -- a program for painting on the screen
  * with this program you can play, draw, learn and teach
  * This program has been written such as a freedom sonet
@@ -10,12 +10,12 @@
  * under the terms of the GNU General Public License as published by the
  * Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * Ardesia is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along
  * with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
@@ -29,6 +29,7 @@
 #include <gtk/gtk.h>
 
 #include <cairo.h>
+#include <ardesia.h>
 
 #ifdef _WIN32
 #  include <cairo-win32.h>
@@ -60,7 +61,7 @@ typedef enum
     ANNOTATE_PEN,
 
     ANNOTATE_ERASER,
-    
+
     ANNOTATE_FILLER,
 
   } AnnotatePaintType;
@@ -87,6 +88,8 @@ typedef struct _AnnotateSavePoint
 
 typedef struct
 {
+  /* number of points added to coord_list */
+  guint length;
 
   /* List of the coordinates of the last line drawn. */
   GSList       *coord_list;
@@ -102,7 +105,11 @@ typedef struct
 /* Annotation data used by the callbacks. */
 typedef struct
 {
-
+  gboolean is_background_visible;
+  gboolean is_text_editor_visible;
+  gboolean is_annotation_visible;
+  gboolean is_window_covering_toolbar;
+  gboolean is_opaque;
   /* Gtkbuilder for annotation window. */
   GtkBuilder *annotation_window_gtk_builder;
 
@@ -141,13 +148,13 @@ typedef struct
 
   /* Paint context for the eraser. */
   AnnotatePaintContext *default_eraser;
-  
+
   /* Paint context for the filler. */
   AnnotatePaintContext *default_filler;
 
   /* Point to the current context. */
   AnnotatePaintContext *cur_context;
-  
+
   /*
    * This store the old paint type tool;
    * it is used to switch from/to eraser/pen
@@ -160,7 +167,7 @@ typedef struct
 
   /* Is the rectify mode enabled? */
   gboolean     rectify;
-  
+
   /* Is the roundify mode enabled?*/
   gboolean     roundify;
 
@@ -179,20 +186,31 @@ typedef struct
   /* Pen color. */
   gchar *color;
 
+
+  /* monitor */
+  Monitor* monitor;
 } AnnotateData;
 
+AnnotateData* annotation_data;
 
 /* Initialize the annotation cairo context */
 void
 initialize_annotation_cairo_context (AnnotateData *data);
 
+GtkWidget *
+create_annotation_window();
+
+void
+position_annotation_window();
 
 /* Initialize the annotation window. */
-gint
-annotate_init                (GtkWidget *parent,
-                              gchar     *iwb_filename,
-                              gboolean   debug);
+void
+annotate_init                (gchar     *iwb_filename,
+                              gboolean   debug,
+                              Monitor*  monitor);
 
+void
+annotation_window_change(int width, int height);
 
 /* Set-up input device. */
 void
@@ -356,8 +374,8 @@ annotate_draw_point          (AnnotateDeviceData *devdata,
 void
 annotate_draw_point_list     (AnnotateDeviceData *devdata,
                               GSList             *list);
-                              
-                              
+
+
 /* Draw an arrow using some polygons. */
 void
 annotate_draw_arrow          (AnnotateDeviceData *devdata,
@@ -370,8 +388,8 @@ annotate_fill                (AnnotateDeviceData *devdata,
                               AnnotateData       *data,
                               gdouble             x,
                               gdouble             y);
-                              
-                              
+
+
 /* Select eraser, pen or other tool for tablet. */
 void
 annotate_select_tool         (AnnotateData *data,
@@ -415,7 +433,13 @@ annotate_add_savepoint       ();
 void
 annotate_configure_pen_options    (AnnotateData       *data);
 
+gboolean
+annotation_window_button_press(GdkEventButton* ev, AnnotateData* data);
+
+gboolean
+annotation_window_mouse_move( GdkEventMotion* ev, AnnotateData* data ) ;
+
+gboolean
+annotation_window_button_release( GdkEventButton* ev, AnnotateData* data);
 
 #endif
-
-

src/ardesia.c
@@ -1,4 +1,4 @@
-/* 
+/*
  * Ardesia -- a program for painting on the screen
  * with this program you can play, draw, learn and teach
  * This program has been written such as a freedom sonet
@@ -10,70 +10,99 @@
  * under the terms of the GNU General Public License as published by the
  * Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * Ardesia is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along
  * with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
 
-
+#include <ardesia.h>
 #include <utils.h>
+#include <bar_callbacks.h>
 #include <annotation_window.h>
 #include <background_window.h>
 #include <project_dialog.h>
 #include <bar.h>
+#include <commandline.h>
+#include <text_window.h>
 
 /*ch* External defined structure used to configure text input. (see text_window.c) */
 #include <text_window.h>
 extern TextConfig *text_config;
+GtkWidget   *ardesia_bar_window;
+GtkWidget   *background_window;
+GtkWidget   *annotation_window;
+Workspace   *workspace;
+CommandLine* commandline = NULL;
 
-/* Print the version of the tool and exit. */
-static void
-print_version      ()
-{
-  g_printf ("Ardesia %s; the free digital sketchpad\n\n", PACKAGE_VERSION);
-  exit (EXIT_SUCCESS);
+
+GdkRectangle* get_toolbar_area() {
+    if ( commandline != NULL ) {
+        if ( commandline->mode == DRAW_ON_MONITOR ) {
+            Monitor* monitor = g_list_nth_data( workspace->monitors, commandline->tools_monitor );
+            return monitor->rect;
+        } else {
+            //gtk_getet_display(commandline->clipRect->x, commandline->clipRect->y);
+        }
+    }
+    return NULL;
 }
 
-
-/* Print the command line help. */
-static void
-print_help         ()
-{
-  gchar *year = "2009-2010";
-  gchar *author = "Pietro Pilolli";
-  g_printf ("Usage: %s [options] [filename]\n\n", PACKAGE_NAME);
-  g_printf ("Ardesia the free digital sketchpad\n\n");
-  g_printf ("options:\n");
-  g_printf ("  --verbose ,\t-V\t\tEnable verbose mode to see the logs\n");
-  g_printf ("  --decorate,\t-d\t\tDecorate the window with the borders\n");
-  g_printf ("  --gravity ,\t-g\t\tSet the gravity of the bar. Possible values are:\n");
-  g_printf ("  \t\t\t\teast [default]\n");
-  g_printf ("  \t\t\t\twest\n");
-  g_printf ("  \t\t\t\tnorth\n");
-  g_printf ("  \t\t\t\tsouth\n");
-  g_printf ("  --font ,\t-f\t\tSet the font family for the text window. Possible values are:\n");
-  g_printf ("  \t\t\t\tserif [default]\n");
-  g_printf ("  \t\t\t\tsans-serif\n");
-  g_printf ("  \t\t\t\tmonospace\n");
-  g_printf ("  --leftmargin,\t-l\t\tSet the left margin in text window to set after hitting Enter\n");
-  g_printf ("  --tabsize,\t-t\t\tSet the tabsize in pixel in text window\n");
-  g_printf ("  --help    ,\t-h\t\tShows the help screen\n");
-  g_printf ("  --version ,\t-v\t\tShows version information and exit\n");
-  g_printf ("\n");
-  g_printf ("filename:\t  \t\tThe interactive Whiteboard Common File (iwb)\n");
-  g_printf ("\n");
-  g_printf ("%s (C) %s %s\n", PACKAGE_STRING, year, author);
-  exit (EXIT_SUCCESS);
+/**
+ * Get the drawable area for annotation, text and background windows
+ * @return NULL if not set, GdkRectangle if it is
+ */
+GdkRectangle* get_drawable_area() {
+    if ( commandline != NULL ) {
+        if ( commandline->mode == DRAW_ON_MONITOR ) {
+            if ( commandline->workspace_monitor < 0 || commandline->workspace_monitor >= g_list_length(workspace->monitors) ) {
+                g_warning("Workspace monitor was given an illegal value, moving to monitor 0.\n");
+                commandline->workspace_monitor = 0;
+            }
+            Monitor* monitor = g_list_nth_data( workspace->monitors, commandline->workspace_monitor );
+            return monitor->rect;
+        } else if ( commandline->mode == DRAW_ON_FULLDESKTOP ) {
+            GdkWindow* rootwindow = gdk_screen_get_root_window( gdk_screen_get_default() );
+            int maxwidth = gdk_window_get_width( rootwindow );
+            int maxheight = gdk_window_get_height( rootwindow );
+            commandline->clipRect->x = 0;
+            commandline->clipRect->y = 0;
+            commandline->clipRect->width = maxwidth;
+            commandline->clipRect->height = maxheight;
+            return commandline->clipRect;
+        } else {
+            // check clipRect bounds
+            GdkWindow* rootwindow = gdk_screen_get_root_window( gdk_screen_get_default() );
+            int maxwidth = gdk_window_get_width( rootwindow );
+            int maxheight = gdk_window_get_height( rootwindow );
+            g_printf("Maximum Size: %d %d\n", maxwidth, maxheight);
+            if ( commandline->clipRect->x < 0 ) {
+                commandline->clipRect->x = 0;
+            }
+            if ( commandline->clipRect->y < 0 ) {
+                commandline->clipRect->y = 0;
+            }
+            if ( commandline->clipRect->x > maxwidth ) {
+                commandline->clipRect->x = maxwidth;
+            }
+            if ( commandline->clipRect->x > maxheight ) {
+                commandline->clipRect->y = maxheight;
+            }
+            return commandline->clipRect;
+        }
+    }
+    return NULL;
 }
+
 
 
 #ifndef _WIN32
+
 /* Call the dialog that inform the user to enable a composite manager. */
 static void
 run_missing_composite_manager_dialog   ()
@@ -112,121 +141,8 @@ check_composite_manager      ()
     }
 
 }
+
 #endif
-
-
-/* Parse the command line in the standard getopt way. */
-static CommandLine *
-parse_options           (gint   argc,
-                         char  *argv[])
-{
-  CommandLine *commandline = g_malloc ((gsize) sizeof (CommandLine));
-
-  commandline->position = EAST;
-  commandline->debug = FALSE;
-  commandline->iwb_filename = NULL;
-  commandline->decorated=FALSE;
-  commandline->fontfamily = "serif";
-  commandline->text_leftmargin = 0;
-  commandline->text_tabsize = 80;
-
-  /* Getopt_long stores the option index here. */
-  while (1)
-    {
-      gint c;
-      static struct option long_options[] =
-      {/* These options set a flag. */
-      {"help", no_argument,       0, 'h'},
-      {"decorated", no_argument,  0, 'd'},
-      {"verbose", no_argument,    0, 'V'},
-      {"version", no_argument,    0, 'v'},
-      /*
-       * These options don't set a flag.
-       * We distinguish them by their indices.
-       */
-      {"gravity", required_argument, 0, 'g'},
-      {"font", required_argument, 0, 'f'},
-      {"leftmargin", required_argument, 0, 'l'},
-      {"tabsize", required_argument, 0, 't'},
-      {0, 0, 0, 0}
-      };
-
-      gint option_index = 0;
-      c = getopt_long (argc,
-                       argv,
-                       "hdvVg:f:l:t:",
-                       long_options,
-                       &option_index);
-
-      /* Detect the end of the options. */
-      if (c == -1)
-        {
-          break;
-        }
-
-      switch (c)
-        {
-          case 'h':
-            print_help ();
-            break;
-          case 'v':
-            print_version ();
-            break;
-          case 'd':
-            commandline->decorated=TRUE;
-            break;
-          case 'V':
-            commandline->debug=TRUE;
-            break;
-          case 'g':
-            if (g_strcmp0 (optarg, "east") == 0)
-              {
-                commandline->position = EAST;
-              }
-            else if (g_strcmp0 (optarg, "west") == 0)
-              {
-                commandline->position = WEST;
-              }
-            else if (g_strcmp0 (optarg, "north") == 0)
-              {
-                commandline->position = NORTH;
-              }
-            else if (g_strcmp0 (optarg, "south") == 0)
-              {
-                commandline->position = SOUTH;
-              }
-            else
-              {
-                print_help ();
-              }
-            break;
-          case 'f':
-            if (g_strcmp0 (optarg, "serif") == 0 ||
-                g_strcmp0 (optarg, "sans-serif") == 0 ||
-                g_strcmp0 (optarg, "monospace") == 0)
-              {
-                commandline->fontfamily = optarg;
-              }
-            break;
-          case 'l':
-            commandline->text_leftmargin = atoi(optarg);
-            break;
-          case 't':
-            commandline->text_tabsize = atoi(optarg);
-            break;
-          default:
-            print_help ();
-            break;
-        }
-    }
-
-  if (optind<argc)
-    {
-      commandline->iwb_filename = argv[optind];
-    }
-
-  return commandline;
-}
 
 
 /* Enable the localization support with gettext. */
@@ -240,68 +156,45 @@ enable_localization_support       ()
 #endif
 }
 
-
-/* Create a shorcut to the workspace on the desktop. */
-static void
-create_workspace_shortcut (gchar *workspace_dir)
-{
-  gchar *desktop_entry_filename = g_strdup_printf ("%s%s%s_workspace",
-                                                    get_desktop_dir (),
-                                                    G_DIR_SEPARATOR_S,
-                                                    PACKAGE_NAME);
-
-#ifdef _WIN32
-  windows_create_link (workspace_dir,
-                       desktop_entry_filename,
-                       "%SystemRoot%\\system32\\imageres.dll",
-                       123);
-
-#else
-  xdg_create_link (workspace_dir , desktop_entry_filename, "folder-documents");
-#endif
-  g_free (desktop_entry_filename);
-}
-
-
-/* Configure the workspace. */
-static gchar *
-configure_workspace               (gchar *project_name)
-{
-  gchar *workspace_dir = (gchar *) NULL;
-  const gchar *documents_dir = get_documents_dir ();
-
-  /* The workspace directory is in the documents ardesia folder. */
-  workspace_dir = g_build_filename (documents_dir,
-                                    PACKAGE_NAME,
-                                    (gchar *) 0);
-
-  create_workspace_shortcut (workspace_dir);
-
-  return workspace_dir;
-}
-
-
-/* Create the default project dir under the workspace_dir. */
-static gchar *
-create_default_project_dir        (gchar  *workspace_dir,
-                                   gchar  *project_name)
-{
-  gchar *project_dir = g_build_filename (workspace_dir,
-                                         project_name,
-                                         (gchar *) 0);
-
-  if (!file_exists (project_dir))
+void
+build_annotation_window() {
+    annotate_init (workspace->iwb_filename,
+                    commandline->debug,
+                    NULL);
+    annotation_data->is_opaque = commandline->is_opaque;
+    annotation_window = create_annotation_window();
+    if (annotation_window == NULL)
     {
-
-      if (g_mkdir_with_parents (project_dir, 0700)==-1)
-        {
-          g_warning ("Unable to create folder %s\n", project_dir);
-        }
-
+        annotate_quit ();
+        g_free (commandline);
+        exit (EXIT_FAILURE);
     }
 
-  return project_dir;
+    GdkRectangle* rect = get_drawable_area();
+    position_annotation_window(rect->x,rect->y,rect->width,rect->height); //rect->width,rect->height);
+
+    gtk_widget_show (annotation_window);
 }
+
+void
+build_toolbar_window() {
+    GdkRectangle* rect = get_toolbar_area();
+    ardesia_bar_window = create_bar_window (commandline, rect, annotation_window);
+
+    if (ardesia_bar_window == NULL)
+      {
+        annotate_quit ();
+        //destroy_background_window ();
+        g_free (commandline);
+        exit (EXIT_FAILURE);
+      }
+
+    gtk_window_set_keep_above (GTK_WINDOW (ardesia_bar_window), TRUE);
+    gtk_widget_show (ardesia_bar_window);
+
+
+}
+
 
 
 /* This is the starting point of the program. */
@@ -309,136 +202,56 @@ int
 main                              (int    argc,
                                    char  *argv[])
 {
-  CommandLine *commandline = (CommandLine *) NULL;
-  gchar       *project_name = (gchar *) NULL;
-  gchar       *project_dir = (gchar *) NULL;
-  gchar       *iwb_filename = (gchar *) NULL;
-  GtkWidget   *background_window = (GtkWidget *) NULL;
-  GtkWidget   *annotation_window = (GtkWidget *) NULL;
-  GtkWidget   *ardesia_bar_window = (GtkWidget *) NULL;
+  // make global variables NULL
+  commandline               = (CommandLine *)   NULL;
+  ardesia_bar_window        = (GtkWidget *)     NULL;
+  background_window         = (GtkWidget *)     NULL;
+  annotation_window         = (GtkWidget *)     NULL;
+  workspace                 = (Workspace*)       NULL;
 
   /* Enable the localization support with gettext. */
   enable_localization_support ();
 
+  // start GTK
   gtk_init (&argc, &argv);
 
 #ifndef _WIN32
   check_composite_manager ();
 #endif
 
-	
-  /*
-   * Uncomment this and the create_segmentation_fault function
-   * to create a segmentation fault.
-   */
-  //create_segmentation_fault ();
+  // handle command line
+  commandline = create_command_line();
+  parse_options(commandline, argc, argv);
+  print_command_line( commandline );
 
-  commandline = parse_options (argc, argv);
-	
   /* Initialize new text configuration options. */
-  text_config = g_malloc ((gsize) sizeof (TextConfig));
+  text_config = create_text_config();
   text_config->fontfamily = commandline->fontfamily;
   text_config->leftmargin = commandline->text_leftmargin;
   text_config->tabsize = commandline->text_tabsize;
 
-  if (commandline->iwb_filename)
-    {
-      gint init_pos = -1;
-      gint end_pos = -1;
+  // handle workspace
+  workspace = create_workspace();
+  if (commandline->iwb_filename) {
+      change_workspace_to( workspace, commandline->iwb_filename );
+  }
+  build_workspace_filesystem(workspace);
 
+  // create windows, one for the drawing and one for the toolbar
+  build_annotation_window();
+  build_toolbar_window();
 
-      if (g_path_is_absolute (commandline->iwb_filename))
-        {
-          iwb_filename = g_strdup (commandline->iwb_filename);
-        }
-      else
-        {
-          gchar *dir = g_get_current_dir ();
-          iwb_filename = g_build_filename (dir, commandline->iwb_filename, (gchar *) 0);
-          g_free (dir);
-        }
+  // move tool bar to appropriate screen
+  //Monitor* toolMonitor = g_list_nth_data( monitors, commandline->toolsMonitor - 1 );
+  //gtk_window_move( GTK_WINDOW(ardesia_bar_window), toolMonitor->rect->x, 0 );
 
-      if (!file_exists(iwb_filename))
-        {
-          printf("No such file %s\n", iwb_filename);
-          exit (EXIT_FAILURE);
-        }
+  replace_status_message(g_strdup_printf("Project started in %s", workspace->project_dir));
 
-      init_pos = g_substrlastpos (iwb_filename, G_DIR_SEPARATOR_S);
-      end_pos  = g_substrlastpos (iwb_filename, ".");
-      project_name = g_substr (iwb_filename, init_pos+1, end_pos-1);
-      project_dir = g_substr (iwb_filename, 0, init_pos-1);
-    }
-  else
-    {
-      gchar *workspace_dir = (gchar *) NULL;
-
-      /* Show the project name wizard. */
-      project_name = start_project_dialog ();
-      workspace_dir = configure_workspace (project_name);
-      project_dir = create_default_project_dir (workspace_dir, project_name);
-      g_free (workspace_dir);
-    }
-
-  set_project_name (project_name);
-  set_project_dir (project_dir);
-  set_iwb_filename (iwb_filename);
-
-
-  background_window = create_background_window ();
-
-  if (background_window == NULL)
-    {
-      destroy_background_window ();
-      g_free (commandline);
-      exit (EXIT_FAILURE);
-    }
-
-  gtk_widget_show (background_window);
-  
-  set_background_window (background_window);
-  
-  /* Initialize the annotation window. */
-  annotate_init (background_window, iwb_filename, commandline->debug);
-
-  annotation_window = get_annotation_window ();
-
-  if (annotation_window == NULL)
-    {
-      annotate_quit ();
-      destroy_background_window ();
-      g_free (commandline);
-      exit (EXIT_FAILURE);
-    }
-
-  /* Postcondition: the annotation window is valid. */
-  gtk_window_set_keep_above (GTK_WINDOW (annotation_window), TRUE);
-  
-  gtk_widget_show (annotation_window);
-  
-  ardesia_bar_window = create_bar_window (commandline, annotation_window);
-
-  if (ardesia_bar_window == NULL)
-    {
-      annotate_quit ();
-      destroy_background_window ();
-      g_free (commandline);
-      exit (EXIT_FAILURE);
-    }
-
-  gtk_window_set_keep_above (GTK_WINDOW (ardesia_bar_window), TRUE);
-  gtk_widget_show (ardesia_bar_window);
+  // main loop for a GTK application
   gtk_main ();
 
-  g_free (project_name);
-
-  remove_dir_if_empty(project_dir);
-
-  g_free (project_dir);
-  
-  g_free (iwb_filename);
-
-  g_free (commandline);
+  destroy_workspace(workspace);
+  destroy_command_line(commandline);
 
   return 0;
 }

commit ae704a05441df903792cecf7765fbca0b6244518

Fix iwb loading broken since e011aab

src/annotation_window.c
@@ -381,13 +381,17 @@ roundify (AnnotateDeviceData *devdata, gboolean closed_path)
 
 /* Create the annotation window. */
 GtkWidget *
-create_annotation_window ()
+create_annotation_window (Workspace *workspace, CommandLine *commandline)
 {
   GtkWidget *widget = (GtkWidget *) NULL;
   GError    *error  = (GError *) NULL;
 
+  annotate_init(NULL);
+
   /* Initialize the main window. */
   annotation_data->annotation_window_gtk_builder = gtk_builder_new ();
+  annotation_data->is_opaque = commandline->is_opaque;
+  
 
   /* Load the gtk builder file created with glade. */
   gtk_builder_add_from_file (annotation_data->annotation_window_gtk_builder,
@@ -410,6 +414,10 @@ create_annotation_window ()
       return NULL;
     }
 
+  if (workspace->iwb_filename)
+    {
+      annotation_data->savepoint_list = load_iwb (workspace->iwb_filename);
+    }
   /* Connect all the callback from gtkbuilder xml file. */
   gtk_builder_connect_signals (annotation_data->annotation_window_gtk_builder,
                                (gpointer) annotation_data);
@@ -706,8 +714,14 @@ initialize_annotation_cairo_context (AnnotateData *data)
 
       if (annotation_data->savepoint_list == NULL)
         {
+          g_debug("It has not savepoint; clear the screen");
           /* Clear the screen and create the first empty savepoint. */
           annotate_clear_screen ();
+        }
+      else
+	{
+          g_debug("It has savepoint; restore surface");
+	  annotate_restore_surface();
         }
 
 #ifndef _WIN32
@@ -1469,7 +1483,6 @@ create_annotation_data ()
 
   annotation_data->is_cursor_hidden = TRUE;
 
-  annotation_data->debug       = FALSE;
   annotation_data->default_pen = annotate_paint_context_new (ANNOTATE_PEN);
   annotation_data->default_eraser = annotate_paint_context_new (ANNOTATE_ERASER);
   annotation_data->default_filler = annotate_paint_context_new (ANNOTATE_FILLER);
@@ -1508,7 +1521,7 @@ create_annotation_data ()
 
 /* Initialize the annotation. */
 void
-annotate_init (gchar *iwb_file, gboolean debug, Monitor *monitor)
+annotate_init (Monitor *monitor)
 {
   cursors_main ();
 
@@ -1516,18 +1529,12 @@ annotate_init (gchar *iwb_file, gboolean debug, Monitor *monitor)
   create_annotation_data ();
 
   /* Initialize the pen context. */
-  annotation_data->debug   = debug;
   annotation_data->monitor = monitor;
 
   setup_input_devices (annotation_data);
   allocate_invisible_cursor (&annotation_data->invisible_cursor);
 
   create_savepoint_dir ();
-
-  if (iwb_file)
-    {
-      annotation_data->savepoint_list = load_iwb (iwb_file);
-    }
 }
 
 gboolean

src/annotation_window.h
@@ -226,9 +226,6 @@ typedef struct
   /* Is the cursor hidden. */
   gboolean is_cursor_hidden;
 
-  /* Is the debug enabled. */
-  gboolean debug;
-
   /* Pen color. */
   gchar *color;
 
@@ -244,12 +241,12 @@ extern AnnotateData *annotation_data;
 /* Initialize the annotation cairo context */
 void initialize_annotation_cairo_context (AnnotateData *data);
 
-GtkWidget *create_annotation_window ();
+GtkWidget *create_annotation_window (Workspace *workspace, CommandLine *commandline);
 
 void position_annotation_window ();
 
 /* Initialize the annotation window. */
-void annotate_init (gchar *iwb_filename, gboolean debug, Monitor *monitor);
+void annotate_init (Monitor *monitor);
 
 void annotation_window_change (int width, int height);
 

src/ardesia.c
@@ -169,9 +169,7 @@ enable_localization_support ()
 void
 build_annotation_window ()
 {
-  annotate_init (workspace->iwb_filename, commandline->debug, NULL);
-  annotation_data->is_opaque = commandline->is_opaque;
-  annotation_window          = create_annotation_window ();
+  annotation_window          = create_annotation_window (workspace, commandline);
   if (annotation_window == NULL)
     {
       annotate_quit ();

src/cairo_functions.c
@@ -97,17 +97,16 @@ void
 load_color_onto_context (gchar *hex_color, cairo_t *cr)
 {
   g_debug ("Load color %s onto context\n", hex_color);
-  assert (hex_color);
-  assert (cr);
-  assert (strlen (hex_color) == 8);
-
-  gint r = 0;
-  gint g = 0;
-  gint b = 0;
-  gint a = 0;
 
   if (cr)
     {
+      assert (hex_color);
+      assert (strlen (hex_color) == 8);
+
+      gint r = 0;
+      gint g = 0;
+      gint b = 0;
+      gint a = 0;
       sscanf (hex_color, "%02X%02X%02X%02X", &r, &g, &b, &a);
       cairo_save (cr);
       cairo_set_operator (cr, CAIRO_OPERATOR_SOURCE);