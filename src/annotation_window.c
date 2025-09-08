pilollipietro/ardesia

commit ed6343059d298db60349baf5d610267381d819cc

disable font settings on startup

src/ardesia.c
@@ -242,7 +242,7 @@ main                              (int    argc,
 
   replace_status_message(g_strdup_printf("Project started in %s", workspace->project_dir));
 
-  create_text_settings_window();
+  //create_text_settings_window();
 
   // main loop for a GTK application
   gtk_main();

commit f17c71891578edd48ac288150a42c3bd2d4922d3

FIx text tool broken since Commit ed63430

src/annotation_window.c
@@ -414,10 +414,6 @@ create_annotation_window (Workspace *workspace, CommandLine *commandline)
       return NULL;
     }
 
-  if (workspace->iwb_filename)
-    {
-      annotation_data->savepoint_list = load_iwb (workspace->iwb_filename);
-    }
   /* Connect all the callback from gtkbuilder xml file. */
   gtk_builder_connect_signals (annotation_data->annotation_window_gtk_builder,
                                (gpointer) annotation_data);
@@ -429,6 +425,10 @@ create_annotation_window (Workspace *workspace, CommandLine *commandline)
   g_signal_connect (seat, "device-added", G_CALLBACK (on_device_added), annotation_data);
   g_signal_connect (seat, "device-removed", G_CALLBACK (on_device_removed), annotation_data);
 
+  if (workspace->iwb_filename)
+    {
+      annotation_data->savepoint_list = load_iwb (workspace->iwb_filename);
+    }
   return widget;
 }
 
@@ -679,7 +679,6 @@ annotate_add_savepoint ()
 void
 initialize_annotation_cairo_context (AnnotateData *data)
 {
-  g_debug ("initialize_annotation_cairo_context\n");
   if (annotation_data->annotation_cairo_context == NULL)
     {
       g_debug ("initializing annotation cairo context\n");
@@ -1349,7 +1348,7 @@ annotate_quit ()
     }
 }
 
-/* Release input grab;the input event will be passed below the window. */
+/* Release input grab; the input event will be passed below the window. */
 void
 annotate_release_input_grab ()
 {
@@ -1873,7 +1872,8 @@ gboolean
 on_font_window_leave_event (GtkWidget *widget, GdkEvent *event, gpointer user_data)
 {
   annotation_data->font = gtk_font_chooser_get_font_desc (GTK_FONT_CHOOSER (user_data));
-  g_printf ("selecting new font\n");
+  g_debug ("selecting new font\n");
+  annotate_acquire_input_grab();
   return FALSE;
 }
 
@@ -1894,8 +1894,7 @@ on_font_window_configure (GtkWidget *widget, GdkEvent *event, gpointer user_data
   return FALSE;
 }
 
-void
-create_text_settings_window ()
+void create_text_settings_window ()
 {
   GtkWidget *window       = NULL;
   GtkWidget *font_chooser = NULL;
@@ -1926,6 +1925,10 @@ create_text_settings_window ()
 
       annotation_data->font_window = window;
     }
+}
 
-  gtk_widget_show_all (window);
+void show_text_settings_window()
+{
+  annotate_release_input_grab();
+  gtk_widget_show_all (annotation_data->font_window);
 }

src/annotation_window.h
@@ -382,5 +382,6 @@ gboolean annotation_window_mouse_move (GdkEventMotion *ev, AnnotateData *data);
 gboolean annotation_window_button_release (GdkEventButton *ev, AnnotateData *data);
 
 void create_text_settings_window ();
+void show_text_settings_window ();
 
 #endif

src/ardesia.c
@@ -249,7 +249,7 @@ main (int argc, char *argv[])
 
   replace_status_message (g_strdup_printf ("Project started in %s", workspace->project_dir));
 
-  // create_text_settings_window();
+  create_text_settings_window();
 
   // main loop for a GTK application
   gtk_main ();

src/bar_callbacks.c
@@ -744,7 +744,7 @@ void create_background_window (/* arguments */)
 G_MODULE_EXPORT void
 on_bar_fonts_clicked (GtkToolButton *toolbutton, gpointer func_data)
 {
-  create_text_settings_window ();
+  show_text_settings_window ();
 }
 
 /* Push preference button. */